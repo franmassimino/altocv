a22e1691e3e9beb8f36e724fbc6bfb656ecfc91b
franmassimino <franmassi1904@gmail.com>
2025-11-19 00:39:31 +0100

    chore: Add .mastra to .gitignore and remove from repository

--- .mastra/output/tools.mjs
+++
@@ -1,4 +0,0 @@
-import * as tool0 from './tools/e6a64c1e-4fa6-42cb-b828-55ad0d9ef5d5.mjs';
-import * as tool1 from './tools/12544204-c495-4036-9780-833990ca63b9.mjs';
-        
-                export const tools = [tool0, tool1]
